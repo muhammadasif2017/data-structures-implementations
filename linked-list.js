af99bdb62460420998f464ff806a6e4b717fe308
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2022-08-21 17:57:26 +0500

    Implemented linked list data structure

---
+++ linked-list.js
@@ -0,0 +1,103 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    if (index <= 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+    if (index >= this.length) {
+      this.append(value);
+      return this.printList();
+    }
+
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter += 1;
+    }
+    return currentNode;
+  }
+
+  lookup(value) {
+    let currentNode = this.head;
+    while (currentNode != null) {
+      if (currentNode.value === value) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  remove(index) {
+    if (index <= 0) {
+      this.head = this.head.next;
+    }
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    unwantedNode.next = null;
+    this.length--;
+    return this.printList();
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 100);
+// console.log(myLinkedList.lookup(1));
+console.log(myLinkedList.printList());
+console.log(myLinkedList.remove(2));
